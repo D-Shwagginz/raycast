8a4cab2fa8439929a9395b569fbe629377629288
D. Shwagginz <mynameisformat@gmail.com>
2026-02-22 02:33:32 -0700

    Architecture changes

--- src/raybuild.cr
+++ src/raybuild.cr
@@ -1,6 +1,16 @@
 require "file_utils"
 
-FileUtils.rm_r("./bin/release")
-puts `xcopy .\\rsrc\\ .\\bin\\release\\ /S /E`
+architecture = ""
+{% if flag?(:arm) || flag?(:aarch64) %}
+  architecture = "arm"
+{% elsif flag?(:x86_64) %}
+  architecture = "x86"
+{% end %}
+
+begin
+  FileUtils.rm_r("./bin/release/#{architecture}")
+rescue File::NotFoundError
+end
+puts `xcopy .\\rsrc\\ .\\bin\\release\\#{architecture}\\ /S /E`
 puts `shards build raycast --release --no-debug`
-FileUtils.cp("./bin/raycast.exe", "./bin/release")
+FileUtils.cp("./bin/raycast.exe", "./bin/release/#{architecture}")
