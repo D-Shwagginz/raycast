c7f1281863e3e32400d6950cd9137ca92d3b3553
D. Shwagginz <mynameisformat@gmail.com>
2026-06-12 04:12:59 -0700

    mac stuff

--- src/raybuild.cr
+++ src/raybuild.cr
@@ -24,6 +24,14 @@ filename = ""
 {% end %}
 
 
-puts `shards build raycast --release --no-debug`
+puts `shards build raycast --release --no-debug `
 
 FileUtils.cp("./bin/#{filename}", "./bin/release/#{architecture}")
+
+{% if flag?(:darwin) %}
+  puts `rm ./bin/release/arm/raylib.dll`
+  puts `install_name_tool -change /opt/homebrew/opt/bdw-gc/lib/libgc.1.dylib "./libgc.1.dylib" ./bin/release/arm/raycast`
+  puts `echo -e "#!/bin/sh\nset -e\ncd ./arm\nexec ./raycast" > ./raycast.sh`
+  puts `platypus -a Raycast -u "Devin Shwagginz" -I org.devinshwagginz.raycast -f ./bin/release/arm -B -R -o "None" ./raycast.sh ./bin/release/arm/Raycast.app`
+  puts `rm ./raycast.sh`
+{% end %}
